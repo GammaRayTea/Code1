6ada0f62e945dc3dfbb22dc15c98063cf9c5cebc
GammaRayTea <162896793+GammaRayTea@users.noreply.github.com>
2025-06-25 13:17:33 +0200

    added notes page for 25_6_25

--- html/Scripts/test.js
+++ html/Scripts/test.js
@@ -1,4 +1,5 @@
 "use strict";
-let v = 1;
-v = v + 1;
-console.log(v);
+let a = { "wert1": true, "wert2": false };
+let vector = { x: 12.4, y: -7.2, meaning: "Ortsvektor" };
+let students = [];
+students.push({ name: "Big Brain", matrikel: 123456, grades: { "EIA1": 1.3, "EIA2": 1.0 } });
--- html/Scripts/test.ts
+++ html/Scripts/test.ts
@@ -1,3 +1,24 @@
-let v = 1;
-v = v + 1;
-console.log(v);
+interface MapStringToBoolean {
+    [key: string]: boolean;
+}
+let a: MapStringToBoolean = {"wert1": true, "wert2": false};
+
+
+
+interface VectorWithMeaning {
+    x: number;
+    y: number;
+    meaning: string;
+}
+let vector: VectorWithMeaning = {x: 12.4, y: -7.2, meaning: "Ortsvektor"};
+
+
+
+interface Student {
+    name: string;
+    matrikel: number;
+    grades: {[module: string]: number};
+}
+let students: Student[] = [];
+
+students.push({name: "Big Brain", matrikel: 123456, grades: {"EIA1": 1.3, "EIA2": 1.0}});
