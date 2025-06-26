14c67b5f888ad3c5ac8d9fff04c5cd5c400befca
GammaRayTea <162896793+GammaRayTea@users.noreply.github.com>
2025-06-26 11:08:28 +0200

    moved smaller task scripts to subdirectory "if" statement example script

---
+++ html/Scripts/small tasks/iftest.js
@@ -0,0 +1,10 @@
+"use strict";
+let x = 2;
+if (x == 1) {
+    console.log("Hello");
+}
+else {
+    console.log("Goodbye");
+}
+console.log("Done");
+//# sourceMappingURL=iftest.js.map
---
+++ html/Scripts/small tasks/iftest.ts
@@ -0,0 +1,9 @@
+
+let x: number = 2;
+if (x == 1) {
+    console.log("Hello");
+}
+else {
+    console.log("Goodbye");
+}
+console.log("Done");
