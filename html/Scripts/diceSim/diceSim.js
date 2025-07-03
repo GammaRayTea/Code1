bd243f47128f75315b76911cfa73c0f1e5a1c65b
GammaRayTea <162896793+GammaRayTea@users.noreply.github.com>
2025-07-03 14:12:55 +0200

    diceSim finalisation 2

--- html/Scripts/diceSim/diceSim.js
+++ html/Scripts/diceSim/diceSim.js
@@ -8,6 +8,7 @@ var diceSim;
     let globalMedian;
     let listOfMedians;
     let rollsSimulated;
+    let cachedRollInstructions;
     initialise();
     function initialise() {
         globalSum = 0;
@@ -17,40 +18,219 @@ var diceSim;
         globalMedian = 0;
         listOfMedians = [];
         rollsSimulated = 0;
-        mainLoop();
+        mainLoop(true);
     }
-    function mainLoop() {
-        let rollInstructions = inputRollInstructions();
+    function mainLoop(_reset) {
+        let rollInstructions;
+        if (_reset) {
+            rollInstructions = inputRollInstructions();
+            cachedRollInstructions = rollInstructions;
+        }
+        else {
+            rollInstructions = cachedRollInstructions;
+        }
         let rollAmount = inputRollAmountAndValidate();
+        rollLoop(rollInstructions, rollAmount);
     }
+    function rollLoop(_rollInstructions, _rollAmount) {
+        let validatedInstructions = parseInstructions(_rollInstructions);
+        //console.log(validatedInstructions);
+        if (validatedInstructions == null) {
+            onValidationFailed("rollInstructions");
+        }
+        else if (_rollAmount == null) {
+            onValidationFailed("rollAmount");
+        }
+        else {
+            rollsSimulated += _rollAmount;
+            let currentListOfSums = [];
+            while (_rollAmount > 0) {
+                let rollResult = simulateRoll(validatedInstructions);
+                currentListOfSums.push(rollResult);
+                updateStats(rollResult);
+                _rollAmount--;
+            }
+            globalMedian = chooseGlobalMedian(currentListOfSums);
+            displayStats();
+        }
+    }
+    //input and parsing
     function inputRollInstructions() {
         let input = prompt("Please Enter the rolls to execute. \n " +
             "They should be in the format \n " +
             "[amount]d[dice type], [amount]d[dice type],... \n" +
             "example: 1d4, 5d6, 3d12");
-        return validateInstructions(parseInstructions(input));
+        return input;
     }
-    function validateInstructions(_parsedInstructions) {
+    function inputRollAmountAndValidate() {
+        let input = Number(prompt("Please enter the number of times these dice should be thrown"));
+        if (input < 1 || Number.isNaN(input) || Number.isInteger(input) == false) {
+            return null;
+        }
+        else {
+            return input;
+        }
     }
     function parseInstructions(_packedInstructions) {
         if (_packedInstructions == null) {
             return null;
         }
-        else
-            : {
-                let parsedInstructions;
-                parsedInstructions = _packedInstructions.split(",");
-                console.log(parsedInstructions);
-                return parsedInstructions;
+        else {
+            let parsedInstructions = [];
+            let splitInstructions = _packedInstructions.replaceAll(" ", "").split(",");
+            // console.log(_packedInstructions.replaceAll(" ", ""))
+            // console.log(splitInstructions);
+            for (let instruction of splitInstructions) {
+                let validatedStep = validateInstructionStep(instruction);
+                if (validatedStep[1]) {
+                    parsedInstructions.push(validatedStep[0]);
+                }
+                else {
+                    return null;
+                }
             }
+            return parsedInstructions;
+        }
     }
-    function inputRollAmountAndValidate() {
-        let input = Number(prompt("Please enter the number of times these dice should be thrown"));
-        if (input < 1 || Number.isNaN(input)) {
-            return null;
+    //validation
+    function validateInstructionStep(_instruction) {
+        let splitInstrucion;
+        let finalInstructions = [];
+        if (_instruction.includes("d")) {
+            splitInstrucion = _instruction.split("d");
+            for (var value of splitInstrucion) {
+                value = Number(value);
+                if (Number.isNaN(value) || !Number.isInteger(value)) {
+                    console.log("not number or integer");
+                    return [[], false];
+                }
+                else {
+                    finalInstructions.push(value);
+                }
+            }
+            return [finalInstructions, true];
         }
         else {
-            return input;
+            console.log("no d");
+            return [[], false];
+        }
+    }
+    function onValidationFailed(_reason) {
+        switch (_reason) {
+            case "rollAmount": {
+                console.log("roll amount invalid");
+                mainLoop();
+                break;
+            }
+            case "rollInstructions": {
+                console.log("roll instr invalid");
+                mainLoop();
+                break;
+            }
+        }
+    }
+    //simulation
+    function simulateRoll(_rollInstructions) {
+        let currentRollSum = 0;
+        for (var instruction of _rollInstructions) {
+            for (let i = 0; i < instruction[0]; i++) { //roll mulitple of same die
+                currentRollSum += randomInt(1, instruction[1]); //add result of one die to sum of rolled dice
+            }
+        }
+        return currentRollSum;
+        function randomInt(min, max) {
+            return min + Math.floor((max - min + 1) * Math.random());
+        }
+    }
+    //statistics
+    function updateStats(_currentRollSum) {
+        globalSum += _currentRollSum;
+        globalMaxRoll = compareMaxRoll(_currentRollSum);
+        globalMinRoll = compareMinRoll(_currentRollSum);
+        globalAverage = calculateAverage(_currentRollSum);
+        globalMedian = 0;
+        listOfMedians = [];
+    }
+    function compareMaxRoll(_currentRollSum) {
+        if (globalMaxRoll < _currentRollSum) {
+            return _currentRollSum;
+        }
+        else {
+            return globalMaxRoll;
+        }
+    }
+    function compareMinRoll(_currentRollSum) {
+        if (globalMinRoll > _currentRollSum || globalMinRoll == 0) {
+            return _currentRollSum;
+        }
+        else {
+            return globalMinRoll;
+        }
+    }
+    function calculateAverage(_currentRollSum) {
+        if (globalAverage == 0) {
+            return _currentRollSum;
+        }
+        else {
+            return (globalAverage + _currentRollSum) / 2;
+        }
+    }
+    function chooseGlobalMedian(_listOfSums) {
+        if (globalMedian == 0) {
+            let median = calculateMedian(_listOfSums);
+            listOfMedians.push(median);
+            return median;
+        }
+        else {
+            listOfMedians.push(calculateMedian(_listOfSums));
+            return calculateMedian(listOfMedians);
+        }
+        function calculateMedian(_numbersToSort) {
+            var half = Math.floor(_numbersToSort.length / 2);
+            _numbersToSort.sort(function (a, b) { return a - b; });
+            if (_numbersToSort.length % 2) {
+                return _numbersToSort[half];
+            }
+            else {
+                return (_numbersToSort[half - 1] + _numbersToSort[half]) / 2.0;
+            }
+        }
+    }
+    function displayStats() {
+        alert(`
+            The sum of all simulated rolls with the selected dice is ${globalSum}.
+            The average is ${globalAverage}.
+            The median sum is ${globalMedian}.
+            The highest recorded roll ist ${globalMaxRoll}.
+            The lowest recorded roll ist ${globalMinRoll}.
+            ${rollsSimulated} roll(s) with the selected dice have been simulated.
+            `);
+        askReset();
+    }
+    function askReset() {
+        let resetInput = Number(prompt("Would you like to (1)reset, (2)reroll the same dice or (3)end."));
+        if (Number.isNaN(resetInput)) {
+            alert("select a valid option");
+            askReset();
+        }
+        else {
+            switch (resetInput) {
+                case 1: {
+                    initialise();
+                    break;
+                }
+                case 2: {
+                    mainLoop(false);
+                    break;
+                }
+                case 3: {
+                    break;
+                }
+                default: {
+                    alert("select a valid option");
+                    askReset();
+                }
+            }
         }
     }
 })(diceSim || (diceSim = {}));
--- html/Scripts/diceSim/diceSim.ts
+++ html/Scripts/diceSim/diceSim.ts
@@ -1,3 +1,4 @@
+
 namespace diceSim {
     let globalSum: number;
     let globalMaxRoll: number;
@@ -6,7 +7,7 @@ namespace diceSim {
     let globalMedian: number;
     let listOfMedians: number[];
     let rollsSimulated: number;
-
+    let cachedRollInstructions: string | null;
 
     initialise();
 
@@ -23,54 +24,78 @@ namespace diceSim {
         listOfMedians = [];
         rollsSimulated = 0;
 
-        mainLoop();
+        mainLoop(true);
     }
 
 
 
 
-    function mainLoop() {
-        let rollInstructions: string | null = inputRollInstructions();
+    function mainLoop(_reset: boolean) {
+        let rollInstructions: string | null;
+        if (_reset) {
+            rollInstructions = inputRollInstructions();
+            cachedRollInstructions = rollInstructions;
+        }
+        else {
+            rollInstructions = cachedRollInstructions;
+        }
+
         let rollAmount: number | null = inputRollAmountAndValidate();
-
-
+        rollLoop(rollInstructions, rollAmount);
     }
 
-    function inputRollInstructions() :string | null {
-        let input:string | null = prompt(
+
+
+
+    function rollLoop(_rollInstructions: string | null, _rollAmount: number | null) {
+
+        let validatedInstructions = parseInstructions(_rollInstructions);
+        //console.log(validatedInstructions);
+        if (validatedInstructions == null) {
+            onValidationFailed("rollInstructions");
+        }
+        else if (_rollAmount == null) {
+            onValidationFailed("rollAmount");
+
+        }
+        else {
+
+            rollsSimulated += _rollAmount;
+            let currentListOfSums: number[] = [];
+            while (_rollAmount > 0) {
+                let rollResult: number = simulateRoll(validatedInstructions);
+                currentListOfSums.push(rollResult);
+                updateStats(rollResult);
+                _rollAmount--;
+
+            }
+            globalMedian = chooseGlobalMedian(currentListOfSums);
+            displayStats();
+
+        }
+    }
+
+
+
+    //input and parsing
+    function inputRollInstructions(): string | null {
+        let input: string | null = prompt(
             "Please Enter the rolls to execute. \n " +
             "They should be in the format \n " +
             "[amount]d[dice type], [amount]d[dice type],... \n" +
             "example: 1d4, 5d6, 3d12"
         );
-        return validateInstructions(parseInstructions(input));
-    }
-
-    function validateInstructions(_parsedInstructions:string[] | null){
-
+        return input;
     }
 
 
-    function parseInstructions(_packedInstructions:string | null) :string[] | null{
-        if (_packedInstructions == null){
-            return null
-        }
-        else: {
-            let parsedInstructions:string[];
-            parsedInstructions = _packedInstructions.split(",");
-            console.log(parsedInstructions);
-            return parsedInstructions;
 
-        }
-        
 
-        
-    }
     function inputRollAmountAndValidate() {
         let input: number = Number(prompt("Please enter the number of times these dice should be thrown"));
 
-        if (input < 1 || Number.isNaN(input)) {
-            return null
+        if (input < 1 || Number.isNaN(input) || Number.isInteger(input) == false) {
+            return null;
         }
         else {
             return input;
@@ -81,4 +106,235 @@ namespace diceSim {
 
 
 
+    function parseInstructions(_packedInstructions: string | null): number[][] | null {
+        if (_packedInstructions == null) {
+            return null
+        }
+        else {
+            let parsedInstructions = [];
+            let splitInstructions: string[] = _packedInstructions.replaceAll(" ", "").split(",");
+            // console.log(_packedInstructions.replaceAll(" ", ""))
+            // console.log(splitInstructions);
+            for (let instruction of splitInstructions) {
+
+                let validatedStep = validateInstructionStep(instruction);
+                if (validatedStep[1]) {
+                    parsedInstructions.push(validatedStep[0]);
+                }
+                else {
+                    return null;
+                }
+            }
+            return parsedInstructions;
+        }
+    }
+
+
+
+    //validation
+    function validateInstructionStep(_instruction: string): [step: number[], valid: boolean] {
+        let splitInstrucion: any[];
+        let finalInstructions: number[] = [];
+
+        if (_instruction.includes("d")) {
+            splitInstrucion = _instruction.split("d");
+
+            for (var value of splitInstrucion) {
+
+                value = Number(value);
+                if (Number.isNaN(value) || !Number.isInteger(value)) {
+                    console.log("not number or integer");
+                    return [[], false];
+                }
+                else {
+                    finalInstructions.push(value);
+                }
+            }
+            return [finalInstructions, true];
+
+        }
+        else {
+            console.log("no d");
+            return [[], false];
+
+        }
+    }
+
+
+
+
+
+
+
+
+    function onValidationFailed(_reason: string) {
+        switch (_reason) {
+            case "rollAmount": {
+                console.log("roll amount invalid");
+                mainLoop();
+                break;
+            }
+            case "rollInstructions": {
+                console.log("roll instr invalid");
+                mainLoop();
+                break;
+            }
+        }
+    }
+
+
+
+
+    //simulation
+    function simulateRoll(_rollInstructions: number[][]): number {
+        let currentRollSum: number = 0;
+
+        for (var instruction of _rollInstructions) {
+            for (let i = 0; i < instruction[0]; i++) { //roll mulitple of same die
+                currentRollSum += randomInt(1, instruction[1]); //add result of one die to sum of rolled dice
+            }
+
+        }
+        return currentRollSum;
+
+
+
+        function randomInt(min: number, max: number) {
+            return min + Math.floor((max - min + 1) * Math.random());
+        }
+    }
+
+    //statistics
+
+
+    function updateStats(_currentRollSum: number) {
+        globalSum += _currentRollSum;
+
+
+
+        globalMaxRoll = compareMaxRoll(_currentRollSum);
+        globalMinRoll = compareMinRoll(_currentRollSum);
+        globalAverage = calculateAverage(_currentRollSum);
+        globalMedian = 0;
+        listOfMedians = [];
+    }
+
+
+
+
+    function compareMaxRoll(_currentRollSum: number) {
+        if (globalMaxRoll < _currentRollSum) {
+            return _currentRollSum;
+        }
+        else {
+            return globalMaxRoll;
+        }
+    }
+
+
+
+
+    function compareMinRoll(_currentRollSum: number) {
+        if (globalMinRoll > _currentRollSum || globalMinRoll ==0) {
+            return _currentRollSum;
+        }
+        else {
+            return globalMinRoll;
+        }
+    }
+
+
+
+
+    function calculateAverage(_currentRollSum: number): number {
+        if (globalAverage == 0) {
+            return _currentRollSum;
+        }
+        else {
+            return (globalAverage + _currentRollSum) / 2
+        }
+    }
+
+
+
+
+    function chooseGlobalMedian(_listOfSums: number[]) {
+        if (globalMedian == 0) {
+            let median = calculateMedian(_listOfSums);
+            listOfMedians.push(median);
+            return median;
+        }
+        else {
+            listOfMedians.push(calculateMedian(_listOfSums));
+            return calculateMedian(listOfMedians);
+        }
+
+
+
+
+
+        function calculateMedian(_numbersToSort: number[]): number {
+
+            var half = Math.floor(_numbersToSort.length / 2);
+            _numbersToSort.sort(function (a, b) { return a - b; });
+
+            if (_numbersToSort.length % 2) {
+                return _numbersToSort[half];
+            } else {
+                return (_numbersToSort[half - 1] + _numbersToSort[half]) / 2.0;
+            }
+
+        }
+    }
+
+
+
+
+    function displayStats() {
+        alert(`
+            The sum of all simulated rolls with the selected dice is ${globalSum}.
+            The average is ${globalAverage}.
+            The median sum is ${globalMedian}.
+            The highest recorded roll ist ${globalMaxRoll}.
+            The lowest recorded roll ist ${globalMinRoll}.
+            ${rollsSimulated} roll(s) with the selected dice have been simulated.
+            `);
+        askReset();
+    }
+
+
+
+
+    function askReset() {
+        let resetInput = Number(prompt("Would you like to (1)reset, (2)reroll the same dice or (3)end."));
+        if (Number.isNaN(resetInput)) {
+            alert("select a valid option");
+            askReset()
+        }
+        else {
+
+            switch (resetInput) {
+                case 1: {
+                    initialise();
+                    break;
+                }
+                case 2: {
+                    mainLoop(false);
+                    break;
+                }
+                case 3: {
+                    break;
+                }
+                default: {
+                    alert("select a valid option");
+                    askReset()
+                }
+
+            }
+        }
+
+
+
+
+    }
 }
