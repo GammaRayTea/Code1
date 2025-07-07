025d0944c0290db5194310d79fefba0dbf443340
GammaRayTea <162896793+GammaRayTea@users.noreply.github.com>
2025-07-07 10:26:15 +0200

    event inspector: fully implemented everything according to the diagram

--- html/Scripts/eventPractice/eventIntroduction.js
+++ html/Scripts/eventPractice/eventIntroduction.js
@@ -4,9 +4,39 @@ var eventPractice;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         document.addEventListener("mousemove", handleMouseMovement);
+        document.addEventListener("keyup", handleKeyup);
+        document.body.addEventListener("click", handleClick);
+        document.body.addEventListener("keyup", handleKeyup);
         let divs = document.getElementsByClassName("blueBox");
+        for (var value of divs) {
+            console.log(value);
+            value.addEventListener("click", handleClick);
+            value.addEventListener("keyup", handleKeyup);
+        }
     }
     function handleMouseMovement(_event) {
+        setInfoBox(_event);
+    }
+    function handleClick(_event) {
+        logInfo(_event);
+    }
+    function handleKeyup(_event) {
+        logInfo(_event);
+    }
+    function setInfoBox(_event) {
+        let position = [_event.clientX, _event.clientY];
+        let infoBox = document.getElementById("infoBox");
+        infoBox.style.left = `${position[0] + 20}px`;
+        infoBox.style.top = `${position[1] + 20}px`;
+        infoBox.innerText = (`position = ${position[0]},${position[1]}
+            target = ${_event.target}`);
+    }
+    function logInfo(_event) {
+        console.log(`type = ${_event.type}
+target = ${_event.target}
+currentTarget = ${_event.currentTarget}
+event Object = ${_event}
+            `);
     }
 })(eventPractice || (eventPractice = {}));
 //# sourceMappingURL=eventIntroduction.js.map
--- html/Scripts/eventPractice/eventIntroduction.ts
+++ html/Scripts/eventPractice/eventIntroduction.ts
@@ -2,14 +2,56 @@ namespace eventPractice {
     window.addEventListener("load", handleLoad);
 
     function handleLoad(_event: Event): void {
+
         document.addEventListener("mousemove", handleMouseMovement);
+        document.addEventListener("keyup", handleKeyup);
+
+        document.body.addEventListener("click", handleClick);
+        document.body.addEventListener("keyup", handleKeyup);
 
         let divs = document.getElementsByClassName("blueBox");
-        
+
+        for (var value of divs) {
+            console.log(value)
+            value.addEventListener("click", handleClick);
+            value.addEventListener("keyup", handleKeyup);
+        }
     }
 
 
-    function handleMouseMovement(_event: Event): void {
+    function handleMouseMovement(_event: MouseEvent): void {
+        setInfoBox(_event)
+    }
 
+    function handleClick(_event: Event): void {
+        logInfo(_event)
+    }
+
+    function handleKeyup(_event: Event): void {
+        logInfo(_event)
+    }
+
+
+    function setInfoBox(_event: MouseEvent): void {
+        let position = [_event.clientX, _event.clientY];
+        let infoBox = document.getElementById("infoBox") as HTMLElement;
+        infoBox.style.left = `${position[0] + 20}px`;
+        infoBox.style.top = `${position[1] + 20}px`;
+        infoBox.innerText = (
+            `position = ${position[0]},${position[1]}
+            target = ${_event.target}`
+        )
+    }
+
+    function logInfo(_event: Event): void {
+        console.log(
+            `type = ${_event.type}
+target = ${_event.target}
+currentTarget = ${_event.currentTarget}
+event Object = ${_event}
+            `
+        )
     }
 }
+
+
