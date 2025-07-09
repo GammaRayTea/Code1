3d4e8358f455e9396249b91da7ffc4d10b789bce
GammaRayTea <162896793+GammaRayTea@users.noreply.github.com>
2025-07-09 09:00:20 +0200

    movingObjects added input based movement

--- html/Scripts/movingObjectsInput/movingObjectsInput.js
+++ html/Scripts/movingObjectsInput/movingObjectsInput.js
@@ -1,8 +1,17 @@
 "use strict";
 var movingObjects;
 (function (movingObjects) {
+    let inputMap = {
+        "shoot": " ",
+        "fwrd": "w",
+        "bwrd": "s",
+        "left": "a",
+        "right": "d",
+        "slow": "Shift"
+    };
+    let registeredInputs = [];
+    let activeInputs = [];
     const tankList = [];
-    let Input = {};
     let viewPortDimensions;
     let myTank = {
         element: document.createElement("span"),
@@ -21,17 +30,50 @@ var movingObjects;
         window.addEventListener("keydown", handleKeyDown);
         window.addEventListener("keyup", handleKeyUp);
         handleResize();
-        setInterval(physicsProcess, 16.6666);
+        setInterval(processLoop, 16.6666);
+        createRegisteredInputList();
     }
-    function physicsProcess() {
+    function processLoop() {
+        pollInputs();
         calcDeltaTime();
         movement();
         doCollision();
     }
     function movement() {
         let deltaDivided = deltaTime / 1000;
-        myTank.rot += 1;
-        myTank.element.style.transform = assembleMatrix(myTank.pos, myTank.rot, { "x": 10, "y": 20 });
+        let slowFactor = 0;
+        let inputVec = { "x": 0, "y": 0 };
+        if (isInputPressed("slow")) {
+            slowFactor = 200;
+        }
+        if (isInputPressed("fwrd")) {
+            inputVec.y += -1;
+            myTank.vel.y = -MAX_SPEED * deltaDivided;
+        }
+        else if (isInputPressed("bwrd")) {
+            inputVec.y += 1;
+            myTank.vel.y = MAX_SPEED * deltaDivided;
+        }
+        if (isInputPressed("left")) {
+            inputVec.x += -1;
+            myTank.vel.x = -MAX_SPEED * deltaDivided;
+        }
+        else if (isInputPressed("right")) {
+            inputVec.x += 1;
+            myTank.vel.x = MAX_SPEED * deltaDivided;
+        }
+        if (inputVec.x != 0 || inputVec.y != 0) {
+            //inputVec = normalise(inputVec);
+        }
+        //myTank.vel.x = inputVec.x * MAX_SPEED* deltaDivided
+        //myTank.vel.y = inputVec.y * MAX_SPEED * deltaDivided
+        myTank.pos.x += myTank.vel.x;
+        myTank.pos.y += myTank.vel.y;
+        console.log(myTank.pos);
+        //apply Transform
+        myTank.element.style.transform = assembleMatrix(myTank.pos, myTank.rot, { "x": 10, "y": 10 });
+        myTank.vel.x = 0;
+        myTank.vel.y = 0;
         previousTime = currentTime;
     }
     function doCollision() {
@@ -49,6 +91,11 @@ var movingObjects;
     function displayFramerate() {
         document.getElementById("frameCounter").innerText = String(Math.round(1000 / deltaTime));
     }
+    //Math
+    function normalise(_vec) {
+        let mag = Math.hypot(_vec.x, _vec.y);
+        return { "x": _vec.x / mag, "y": _vec.y / mag };
+    }
     function randomInt(_min, _max) {
         return _min + Math.floor((_max - _min + 1) * Math.random());
     }
@@ -60,7 +107,6 @@ var movingObjects;
     }
     function handleResize() {
         viewPortDimensions = { "x": window.innerWidth, "y": window.innerHeight };
-        console.log(viewPortDimensions);
     }
     function removeTank(_tankToRemove) {
         for (let tank of tankList) {
@@ -73,12 +119,28 @@ var movingObjects;
     function randomColour() {
         return `rgb(${randomInt(20, 255)},${randomInt(20, 255)},${randomInt(20, 255)})`;
     }
-    //Input polling
+    //Input
+    function isInputPressed(_action) {
+        const keyTyped = _action;
+        return activeInputs.includes(inputMap[keyTyped]);
+    }
     function handleKeyDown(_event) {
+        if (registeredInputs.includes(_event.key) && !activeInputs.includes(_event.key)) {
+            activeInputs.push(_event.key);
+        }
     }
     function handleKeyUp(_event) {
+        activeInputs.splice(activeInputs.indexOf(_event.key), 1);
     }
     function pollInputs() {
+        //console.log(activeInputs);
+        return activeInputs;
+    }
+    function createRegisteredInputList() {
+        let key;
+        for (key in inputMap) {
+            registeredInputs.push(inputMap[key]);
+        }
     }
 })(movingObjects || (movingObjects = {}));
 //# sourceMappingURL=movingObjectsInput.js.map
--- html/Scripts/movingObjectsInput/movingObjectsInput.ts
+++ html/Scripts/movingObjectsInput/movingObjectsInput.ts
@@ -4,8 +4,22 @@ namespace movingObjects {
     type Vector = { x: number, y: number };
 
     type Tank = { element: HTMLElement, pos: Vector, vel: Vector, rot: number };
+
+
+    type inputActions = { "shoot": string, "fwrd": string, "bwrd": string, "left": string, "right": string, "slow": string };
+
+    let inputMap: inputActions = {
+        "shoot": " ",
+        "fwrd": "w",
+        "bwrd": "s",
+        "left": "a",
+        "right": "d",
+        "slow": "Shift"
+    };
+    let registeredInputs: string[] = [];
+    let activeInputs: string[] = [];
     const tankList: Tank[] = [];
-    let Input: {} = {};
+
     let viewPortDimensions: Vector;
     let myTank: Tank = {
         element: document.createElement("span"),
@@ -33,16 +47,18 @@ namespace movingObjects {
         window.addEventListener("keydown", handleKeyDown);
         window.addEventListener("keyup", handleKeyUp);
         handleResize();
-        setInterval(physicsProcess, 16.6666);
+        setInterval(processLoop, 16.6666);
+        createRegisteredInputList();
     }
 
 
 
 
-    function physicsProcess() {
+    function processLoop() {
+        pollInputs();
         calcDeltaTime();
         movement();
-        doCollision()
+        doCollision();
     }
 
 
@@ -54,12 +70,53 @@ namespace movingObjects {
 
     function movement() {
 
-        let deltaDivided = deltaTime / 1000
-        myTank.rot+=1
-        myTank.element.style.transform = assembleMatrix(myTank.pos, myTank.rot, { "x": 10, "y": 20 });
+        let deltaDivided: number = deltaTime / 1000;
+        let slowFactor: number = 0;
+        let inputVec: Vector = { "x": 0, "y": 0 };
 
+        if (isInputPressed("slow")) {
+            slowFactor = 200;
+        }
+        if (isInputPressed("fwrd")) {
+            inputVec.y += -1;
+            myTank.vel.y = -MAX_SPEED * deltaDivided;
+        }
+        else if (isInputPressed("bwrd")) {
+            inputVec.y += 1;
+            myTank.vel.y = MAX_SPEED * deltaDivided;
+        }
+        if (isInputPressed("left")) {
+            inputVec.x += -1;
+            myTank.vel.x = -MAX_SPEED * deltaDivided;
+        }
+        else if (isInputPressed("right")) {
+            inputVec.x += 1;
+            myTank.vel.x = MAX_SPEED * deltaDivided;
+        }
+        if (inputVec.x != 0 || inputVec.y != 0) {
+            //inputVec = normalise(inputVec);
+        }
+        //myTank.vel.x = inputVec.x * MAX_SPEED* deltaDivided
+        //myTank.vel.y = inputVec.y * MAX_SPEED * deltaDivided
+        myTank.pos.x += myTank.vel.x;
+        myTank.pos.y += myTank.vel.y;
+        console.log(myTank.pos);
+
+
+
+        //apply Transform
+        myTank.element.style.transform = assembleMatrix(myTank.pos, myTank.rot, { "x": 10, "y": 10 });
+        myTank.vel.x = 0;
+        myTank.vel.y = 0;
         previousTime = currentTime;
     }
+
+
+
+
+
+
+
 
 
 
@@ -98,6 +155,14 @@ namespace movingObjects {
     }
 
 
+    //Math
+    function normalise(_vec: Vector): Vector {
+        let mag: number = Math.hypot(_vec.x, _vec.y);
+        return { "x": _vec.x / mag, "y": _vec.y / mag };
+    }
+
+
+
 
 
     function randomInt(_min: number, _max: number): number {
@@ -122,7 +187,7 @@ namespace movingObjects {
 
     function handleResize() {
         viewPortDimensions = { "x": window.innerWidth as number, "y": window.innerHeight as number };
-        console.log(viewPortDimensions);
+
     }
 
 
@@ -149,18 +214,48 @@ namespace movingObjects {
 
 
 
+    //Input
+
+    function isInputPressed(_action: string) {
+        const keyTyped = _action as keyof inputActions;
+        return activeInputs.includes(inputMap[keyTyped]);
+    }
 
 
-    //Input polling
+
+
     function handleKeyDown(_event: KeyboardEvent) {
 
+        if (registeredInputs.includes(_event.key) && !activeInputs.includes(_event.key)) {
+            activeInputs.push(_event.key);
+
+        }
     }
+
+
+
+
     function handleKeyUp(_event: KeyboardEvent) {
+        activeInputs.splice(activeInputs.indexOf(_event.key), 1);
 
     }
+
+
 
 
     function pollInputs() {
+        //console.log(activeInputs);
+        return activeInputs;
+    }
 
+
+
+
+    function createRegisteredInputList() {
+        let key: keyof inputActions;
+
+        for (key in inputMap) {
+            registeredInputs.push(inputMap[key]);
+        }
     }
 }
